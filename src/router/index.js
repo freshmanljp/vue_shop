873356249d69df08cfe2629b0ae2af8dce4aad8a
ljp <1498725065@qq.com>
2020-04-28 12:26:56 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,18 @@ import Home from '../components/home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: Login },
-  { path: '/home', component: Home }
+const routes = [{
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
